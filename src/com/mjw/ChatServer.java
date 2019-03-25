f98be7dbc9880d0593586e689938f0a1f7cdc7fb
多喝热水 <692978358@qq.com>
2019-03-25 18:43:16 +0800

    删了两行无关紧要

--- src/com/mjw/ChatServer.java
+++ src/com/mjw/ChatServer.java
@@ -2,9 +2,7 @@ package com.mjw;
 
 
 public class ChatServer{
-
     static final Chat chat = new Chat();
-
     public static void main(String[] args) {
         new Thread(()-> chat.getSocket(6666)).start();
     }
